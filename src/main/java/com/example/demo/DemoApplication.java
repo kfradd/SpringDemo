aa51dd80faccd0b3b1aba13b6975153f69144f71
kfradd <ken@fradd.co.uk>
2021-06-15 21:57:24 +0100

    Slight tweak to greeting

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -15,7 +15,7 @@ public class DemoApplication {
 
     @GetMapping("/hello")
     public String sayHello (@RequestParam(value="myName", defaultValue="World") String name) {
-        return String.format("Hello %s!",name);
+        return String.format("Hello to %s!",name);
     }
 
 
